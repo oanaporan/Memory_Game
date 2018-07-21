3acc5bb32b25b80e4b2e8383d34ecc1ac99705dd
Oana-Poran <oanaporan@gmail.com>
2018-07-21 17:54:25 +0300

    array of card icons added.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
  * Create a list that holds all of your cards
  */
 const cards = document.querySelectorAll('.card');
+const icons = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor","fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle","fa fa-bomb", "fa fa-bomb", "fa fa-bolt", "fa fa-bolt", "fa fa-cube","fa fa-cube"];
 
 /*
  * Display the cards on the page
